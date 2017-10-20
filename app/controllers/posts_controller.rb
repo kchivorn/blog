06f546f2c01d260c7f1a75a018eb8650aa02fa49
kchivorn <kouch.chivorn@gmail.com>
2017-10-20 12:42:41 +0700

    Add Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = t('posts.created')
+      redirect_to @post
+    else
+      flash[:danger] = t('posts.failed')
+      redirect_to :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
