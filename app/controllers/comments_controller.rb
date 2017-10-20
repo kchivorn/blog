e606385779655a3fac8fe7938e8dd6075a957cba
kouch-chivorn <kouchchivorn5@yahoo.com>
2017-10-21 06:53:08 +0700

    Add Comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:create, :destroy]
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def set_comment
+    @post = Post.find(params[:post_id])
+  end
+  def comment_params
+    params.require(:comment).permit(:name, :body)
+  end
+end
